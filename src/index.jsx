4423fd64f01fe313860e0da1c401e142b8f92c70
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-05-11 22:11:53 +0530

    Fixed css issue

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app.jsx";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
+import "./styles.css";
 
 const app = document.getElementById("app");
 const root = createRoot(app);
