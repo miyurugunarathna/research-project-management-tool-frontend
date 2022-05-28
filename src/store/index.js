734c26004852ab680436c76994496199bb2c6327
ShivaaniDushya <it20006884@my.sliit.lk>
2022-05-28 17:06:29 +0530

    Marksheet store created

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter/index.js";
 import submissionReducer from "./submission/index.js";
 import stageReducer from "./stage/index.js";
+import marksheetReducer from "./marksheet/index.js";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     submission: submissionReducer,
     stage: stageReducer,
+    marksheet: marksheetReducer,
   },
 });
 
